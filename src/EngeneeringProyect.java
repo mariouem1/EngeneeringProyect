bf9ca80ef2be784b2c2267bbec0bd31e6135910a
mariouem1 <164615222+mariouem1@users.noreply.github.com>
2024-04-22 20:41:15 +0200

    Update EngeneeringProyect.java

--- src/EngeneeringProyect.java
+++ src/EngeneeringProyect.java
@@ -3,9 +3,9 @@ public class EngeneeringProyect {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("This is an example");
-		System.out.println("This is weeee");
+// Es momento de empezar
+		
+		
 	}
 
 }
