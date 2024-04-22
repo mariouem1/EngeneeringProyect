c588679460192dcd067af544dfd8cc321bb3b8e7
mariouem1 <164615222+mariouem1@users.noreply.github.com>
2024-04-22 21:14:00 +0200

    V1.0.2

--- src/EngeneeringProyect.java
+++ src/EngeneeringProyect.java
@@ -1,13 +1,20 @@
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class EngeneeringProyect {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("This is an example");
-		System.out.println("This is weeee");
-		
-		int xc=0;
+//Comienzo del código
+			
+	try {
+		URL url = new URL ("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/46249?api_key="
+				+ "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJidXN0bWFyaW9Ab3V0bG9vay5jb20iLCJqdGkiOiI4OGM0OTZlMC01MDdjLTQyNjgtOTcxYy00MWNiZDQyMTY3MjciLCJpc3MiOiJBRU1FVCIsImlhdCI6MTcxMzgxMjE5NSwidXNlcklkIjoiODhjNDk2ZTAtNTA3Yy00MjY4LTk3MWMtNDFjYmQ0MjE2NzI3Iiwicm9sZSI6IiJ9.DgMDdxPXiangA3BRy9hToFVNN7WhwobEXkm3NffqhXc");
+	} catch (MalformedURLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}	
+	
 	}
 
 }
