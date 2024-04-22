7c18bfc0ed8f0986f2abe0d5a41c3f8983d76c2f
mariouem1 <164615222+mariouem1@users.noreply.github.com>
2024-04-22 21:26:43 +0200

    V1.0.3

--- src/EngeneeringProyect.java
+++ src/EngeneeringProyect.java
@@ -1,19 +1,36 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class EngeneeringProyect {
 
-	public static void main(String[] args) {
+	private static URL url;
+
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 //Comienzo del código
 			
 	try {
-		URL url = new URL ("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/46249?api_key="
+		URL url = new URL ("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/45210?api_key="
 				+ "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJidXN0bWFyaW9Ab3V0bG9vay5jb20iLCJqdGkiOiI4OGM0OTZlMC01MDdjLTQyNjgtOTcxYy00MWNiZDQyMTY3MjciLCJpc3MiOiJBRU1FVCIsImlhdCI6MTcxMzgxMjE5NSwidXNlcklkIjoiODhjNDk2ZTAtNTA3Yy00MjY4LTk3MWMtNDFjYmQ0MjE2NzI3Iiwicm9sZSI6IiJ9.DgMDdxPXiangA3BRy9hToFVNN7WhwobEXkm3NffqhXc");
 	} catch (MalformedURLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}	
+	HttpURLConnection connection = (HttpURLConnection) url.openConnection();	
+	connection.setRequestMethod("GET");
+	connection.setRequestProperty("Content-Type", "application/json;charset=ISO_8859_1");
+	connection.setDoInput(true);
+	try {
+		connection.connect();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+
+	
+	
 	
 	}
 
