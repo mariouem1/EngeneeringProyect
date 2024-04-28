c7f39c19e6f7ab7da72ae7952d0ecc23ce30cd44
mariouem1 <164615222+mariouem1@users.noreply.github.com>
2024-04-28 15:53:45 +0200

    V1.0.4

--- src/EngeneeringProyect.java
+++ src/EngeneeringProyect.java
@@ -1,18 +1,32 @@
 import java.io.IOException;
+import java.net.CacheResponse;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class EngeneeringProyect {
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
 	private static URL url;
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 //Comienzo del código
-			
+		
 	try {
-		URL url = new URL ("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/diaria/45210?api_key="
+		URL url = new URL ("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/28940?api_key="
 				+ "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJidXN0bWFyaW9Ab3V0bG9vay5jb20iLCJqdGkiOiI4OGM0OTZlMC01MDdjLTQyNjgtOTcxYy00MWNiZDQyMTY3MjciLCJpc3MiOiJBRU1FVCIsImlhdCI6MTcxMzgxMjE5NSwidXNlcklkIjoiODhjNDk2ZTAtNTA3Yy00MjY4LTk3MWMtNDFjYmQ0MjE2NzI3Iiwicm9sZSI6IiJ9.DgMDdxPXiangA3BRy9hToFVNN7WhwobEXkm3NffqhXc");
 	} catch (MalformedURLException e) {
 		// TODO Auto-generated catch block
@@ -28,9 +42,7 @@ public class EngeneeringProyect {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-
-	
-	
+*/
 	
 	}
 
